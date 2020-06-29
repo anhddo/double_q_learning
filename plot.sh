1e338905966ba797db7cc8201d2bfab76295202f
anhdd14 <anhddo93@gmail.com>
2020-06-29 05:11:58 +0000

    add bash file

--- plot.sh
+++ plot.sh
@@ -1,1 +1,1 @@
-python -m june60.plot_result --save-dir tmp/bot-1 --plot-name tmp/image/bot-1-avg.png --avg --height 2.5 --width 20
+python -m june60.plot_result --save-dir tmp/CartPole-v1/7 --plot-name tmp/image/pole-1-avg.png --avg --height 2.5 --width 20
---
+++ take_gpu.sh
@@ -0,0 +1,1 @@
+python -m june60.optimistic_vi.main --buffer 1000 --step 1000 --save-dir tmp/take-gpu --n-run 10000000000000000000 --pause
