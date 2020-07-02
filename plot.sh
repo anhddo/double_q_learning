2bfa995f89c25c649716631d9ed0dae0fe74ac7e
anhdd14 <anhddo93@gmail.com>
2020-07-02 11:50:58 +0700

    update bash file

--- plot.sh
+++ plot.sh
@@ -1,1 +1,4 @@
-python -m june60.plot_result --save-dir tmp/CartPole-v1/7 --plot-name tmp/image/pole-1-avg.png --avg --height 2.5 --width 20
+python -m june60.plot_result --save-dir tmp/CartPole-v1/15 --plot-name tmp/image/pole-1-dqn-line.png --line --height 2.5 --width 20
+
+python -m june60.plot_result --save-dir tmp/CartPole-v1/16 --plot-name tmp/image/pole-1-ddqn-line.png --line --height 2.5 --width 20
+
--- take_gpu.sh
+++ take_gpu.sh
@@ -1,1 +1,1 @@
-python -m june60.optimistic_vi.main --buffer 1000 --step 1000 --save-dir tmp/take-gpu --n-run 10000000000000000000 --pause
+python -m june60.optimistic_vi.main --buffer 10000 --step 30000 --save-dir tmp/haha --n-run 10000000000000000000 --pause
