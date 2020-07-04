6fac0dafd16ae6067aa9a68d624c8e2e24a5bb95
anhdd14 <anhddo93@gmail.com>
2020-07-04 07:06:48 +0000

    update epsilon greedy

--- june60/algo/epsilon_greedy.py
+++ june60/algo/epsilon_greedy.py
@@ -1,14 +1,15 @@
 import numpy.random as npr
 class EpsilonGreedy(object):
-    def __init__(self, max_epsilon, min_epsilon, step, fraction, random_func, action_func):
+    def __init__(self, max_epsilon, min_epsilon, total_step, fraction, random_func, action_func):
         self.min_epsilon = min_epsilon
+        self.max_epsilon = max_epsilon
         self.random_func = random_func
         self.action_func = action_func
-        self.epsilon_decay = (min_epsilon / max_epsilon) ** (1. / (fraction * step))
+        self.epsilon_decay = (min_epsilon / max_epsilon) ** (1. / (fraction * total_step))
         self.epsilon = max_epsilon
 
-    def action(self, state):
-        self.epsilon = max(self.epsilon * self.epsilon_decay, self.min_epsilon)
+    def action(self, state, step):
+        self.epsilon = max(self.max_epsilon * self.epsilon_decay, self.min_epsilon)
         if self.epsilon > npr.uniform():
             action = self.random_func()
         else:
