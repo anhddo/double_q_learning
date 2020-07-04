7366a993e9e68d086d7232f9c74477054eb83a20
anhdd14 <anhddo93@gmail.com>
2020-07-04 14:29:04 +0000

    update pong sh

--- bash/pong.sh
+++ bash/pong.sh
@@ -1,4 +1,4 @@
-python -m june60.deep_rl.atari --tmp-dir tmp --buffer 250000 --step 2000000 \
-    --start 1000 --fraction 0.1 --max-epsilon 1 --min-epsilon 0.1 \
-    --train-step 2 --update 10000 --env Pong-v0 --batch 32 --ddqn \
-    --vi --mse --rms --lr 0.000625 --tau 0.001 --discount 0.99 --tboard
+python -m june60.deep_rl.atari --tmp-dir tmp --buffer 1000000 --step 10000000 \
+    --start 50000 --fraction 0.1 --max-epsilon 1 --min-epsilon 0.1 \
+    --train-step 1 --update 10000 --env PongDeterministic-v4 --batch 32 --ddqn \
+    --vi --huber --rms --lr 0.00025 --tau 0.001 --discount 0.99 --tboard
