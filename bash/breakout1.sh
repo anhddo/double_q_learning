3f205aa24558c971358a40d0085f539c0ad4ed96
anhdd14 <anhddo93@gmail.com>
2020-07-16 03:07:52 +0000

    update bash file

--- bash/breakout1.sh
+++ bash/breakout1.sh
@@ -1,5 +1,5 @@
 python -m june60.deep_rl.atari --tmp-dir ~/tmp --buffer 1000000 --step 100000000 \
-    --start 1000000 --fraction 0.01 --max-epsilon 0.1 --min-epsilon 0.1 \
+    --start 1000000 --fraction 0.1 --max-epsilon 0.1 --min-epsilon 0.1 \
     --train-step 1 --update 10000 --env BreakoutDeterministic-v4 --batch 32 --ddqn \
     --vi --huber --rms --lr 0.00025 --tau 0.001 --discount 0.99 \
-    --load-model-path ~/tmp/BreakoutDeterministic-v4/18/model/32.ckpt
+    --load-model-path ~/tmp/BreakoutDeterministic-v4/2/model/61.ckpt
--- bash/plot_dqn_training.sh
+++ bash/plot_dqn_training.sh
@@ -1,6 +1,8 @@
 while :
 do
-    python -m june60.plot_result --log-path ~/tmp/BreakoutDeterministic-v4/2/logs/1.json 
-    scp ~/tmp/BreakoutDeterministic-v4/2/logs/*.pdf vultr:~/image/breakout
-    sleep 1h
+    python -m june60.plot_result --log-path ~/tmp/BreakoutDeterministic-v4/4/logs/1.json \
+        --width 10 --height 5
+
+    scp ~/tmp/BreakoutDeterministic-v4/4/logs/*.pdf vultr:~/image/breakout
+    sleep 10m
 done
