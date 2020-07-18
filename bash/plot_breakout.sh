57d9875784c60c9f852d3f234526ac6fd9a5630d
anhdd14 <anhddo93@gmail.com>
2020-07-18 11:44:15 +0700

    update bash

--- bash/plot_breakout.sh
+++ bash/plot_breakout.sh
@@ -1,9 +1,9 @@
-DIRPATH=${HOME}/tmp/BreakoutDeterministic-v4/34/logs
+DIRPATH=${HOME}/tmp/BreakoutDeterministic-v4/1/logs
 while :
 do
     if [[ -e ${DIRPATH}/1.json ]]; then
         python -m june60.plot_result --log-path ${DIRPATH}/1.json \
-            --width 10 --height 7
+            --width 10 --height 10 
         scp ${DIRPATH}/*.pdf vultr:~/image/breakout
     fi
     sleep 7m
--- bash/plot_dqn.sh
+++
@@ -1,3 +0,0 @@
-python -m june60.plot_result --log-dir tmp/CartPole-v1/15 tmp/CartPole-v1/16 \
-    --label dqn ddqn\
-    --plot-name tmp/image/pole-1-drl.png --avg --height 2.5 --width 20
