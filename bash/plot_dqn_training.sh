168f84fb811a16aa6e75905e28f9b44059285b8f
anhdd14 <anhddo93@gmail.com>
2020-07-18 10:21:55 +0700

    delete file

--- bash/plot_dqn_training.sh
+++
@@ -1,8 +0,0 @@
-while :
-do
-    python -m june60.plot_result --log-path ~/tmp/BreakoutDeterministic-v4/4/logs/1.json \
-        --width 10 --height 5
-
-    scp ~/tmp/BreakoutDeterministic-v4/4/logs/*.pdf vultr:~/image/breakout
-    sleep 10m
-done
