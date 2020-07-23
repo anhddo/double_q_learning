b173ac9eca894b8736a6f22da93102abff2193f1
anhdd14 <anhddo93@gmail.com>
2020-07-23 15:36:40 +0700

    update epsilon greedy based on final exploration frame

--- june60/algo/epsilon_greedy.py
+++ june60/algo/epsilon_greedy.py
@@ -1,11 +1,11 @@
 import numpy.random as npr
 class EpsilonGreedy(object):
-    def __init__(self, max_epsilon, min_epsilon, total_step, fraction, random_func, action_func):
+    def __init__(self, max_epsilon, min_epsilon, total_step, final_exploration_step, random_func, action_func):
         self.min_epsilon = min_epsilon
         self.max_epsilon = max_epsilon
         self.random_func = random_func
         self.action_func = action_func
-        self.epsilon_decay = (min_epsilon / max_epsilon) ** (1. / (fraction * total_step))
+        self.epsilon_decay = (min_epsilon / max_epsilon) ** (1. / final_exploration_step)
         self.epsilon = max_epsilon
 
     def action(self, state, step):
