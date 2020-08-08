3c4d5be6959c11444e9daba62ecd7b5f0a2fbac4
anhdd14 <anhddo93@gmail.com>
2020-08-08 23:21:40 +0700

    new ps script for training and plotting carpole v1

---
+++ pole-greedy-plot.ps1
@@ -0,0 +1,1 @@
+python -m june60.plot_result --save-dir tmp\CartPole-v1\2 --label greedy --avg --plot-name greedy.png
---
+++ pole-v1-greedy.ps1
@@ -0,0 +1,1 @@
+python -m june60.optimistic_vi.main --env CartPole-v1 --tmp-dir tmp --n-run 2
