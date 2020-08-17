7f72902e83958ef09cf59715b2ad6251ed535b16
anhdd14 <anhddo93@gmail.com>
2020-08-18 00:19:59 +0700

    add function to get epsilon value

--- rl/algo/epsilon_greedy.py
+++ rl/algo/epsilon_greedy.py
@@ -2,35 +2,26 @@ import numpy.random as npr
 
 
 class EpsilonGreedy(object):
-    def __init__(self, args, random_func, action_func):
+    def __init__(self, args, random_func=None, action_func=None):
         self.min_epsilon = args.min_epsilon
         self.max_epsilon = args.max_epsilon
         self.random_func = random_func
         self.action_func = action_func
-        self.epsilon_decay = (self.min_epsilon / self.max_epsilon) ** (1. / args.final_exploration_step)
-        self.epsilon = max_epsilon
+        final_exploration_step = 1
+        if hasattr(args, "final_exploration_step"):
+            final_exploration_step = args.final_exploration_step
+        print(final_exploration_step)
+        self.epsilon_decay = (self.min_epsilon / self.max_epsilon) ** (1. / final_exploration_step)
+        self.epsilon = self.max_epsilon
+
+    def get_epsilon(self, step):
+        epsilon = max(self.max_epsilon * self.epsilon_decay ** step, self.min_epsilon)
+        return epsilon
 
     def action(self, state, step):
-        self.epsilon = max(self.max_epsilon * self.epsilon_decay ** step, self.min_epsilon)
-        if self.epsilon > npr.uniform():
+        epsilon = self.get_epsilon(step)
+        if epsilon > npr.uniform():
             action = self.random_func()
         else:
             action = self.action_func(state)
-        return action, {'epsilon': self.epsilon}
-
-class EpsilonGreedyAgent(object):
-    def __init__(self, agent, args):
-        self.min_epsilon = args.min_epsilon
-        self.max_epsilon = args.max_epsilon
-        self.random_func = random_func
-        self.epsilon_decay = (min_epsilon / max_epsilon) ** (1. / final_exploration_step)
-        self.epsilon = max_epsilon
-        self.agent = agent
-
-    def take_action(self, state, step):
-        self.epsilon = max(self.max_epsilon * self.epsilon_decay ** step, self.min_epsilon)
-        if self.epsilon > npr.uniform():
-            action = self.random_func()
-        else:
-            action = self.action_func(state)
-        return action, {'epsilon': self.epsilon}
+        return action, {'epsilon': epsilon}
