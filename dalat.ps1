3e398a229c5eb692fa3193f40c10822c67de9a44
anhdd14 <anhddo93@gmail.com>
2020-08-28 10:41:17 +0700

    update powerscript

--- dalat.ps1
+++
@@ -1,2 +0,0 @@
-pscp -r june60 rtx-dalat:/home/ubuntu/june60/june60
-pscp -r bash rtx-dalat:/home/ubuntu/june60/bash
--- script/ovi.ps1
+++ script/ovi.ps1
@@ -1,17 +1,20 @@
-python -m rl.optimistic_vi.main --env Acrobot-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 15
-python -m rl.optimistic_vi.main --env Acrobot-v1 --std-vi --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 
-python -m rl.optimistic_vi.main --env Acrobot-v1 --egreedy --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 
+python -m rl.optimistic_vi.main --env Acrobot-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 1
+python -m rl.optimistic_vi.main --env Acrobot-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 5
+python -m rl.optimistic_vi.main --env Acrobot-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 10
+python -m rl.optimistic_vi.main --env Acrobot-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 15
+python -m rl.optimistic_vi.main --env Acrobot-v1 --std-vi --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 
+python -m rl.optimistic_vi.main --env Acrobot-v1 --egreedy --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 
 
-python -m rl.optimistic_vi.main --env CartPole-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 1
-python -m rl.optimistic_vi.main --env CartPole-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 5
-python -m rl.optimistic_vi.main --env CartPole-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 10
-python -m rl.optimistic_vi.main --env CartPole-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 15
-python -m rl.optimistic_vi.main --env CartPole-v1 --std-vi --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 
-python -m rl.optimistic_vi.main --env CartPole-v1 --egreedy --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 
+python -m rl.optimistic_vi.main --env CartPole-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 1
+python -m rl.optimistic_vi.main --env CartPole-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 5
+python -m rl.optimistic_vi.main --env CartPole-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 10
+python -m rl.optimistic_vi.main --env CartPole-v1 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 15
+python -m rl.optimistic_vi.main --env CartPole-v1 --std-vi --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 
+python -m rl.optimistic_vi.main --env CartPole-v1 --egreedy --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 
 
-python -m rl.optimistic_vi.main --env MountainCar-v0 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 1
-python -m rl.optimistic_vi.main --env MountainCar-v0 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 5
-python -m rl.optimistic_vi.main --env MountainCar-v0 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 10
-python -m rl.optimistic_vi.main --env MountainCar-v0 --optimistic --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 --beta 15
-python -m rl.optimistic_vi.main --env MountainCar-v0 --std-vi --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 
-python -m rl.optimistic_vi.main --env MountainCar-v0 --egreedy --tmp-dir tmp --n-run 50 --buffer 5000 --fourier-order 2 
+python -m rl.optimistic_vi.main --env MountainCar-v0 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 1
+python -m rl.optimistic_vi.main --env MountainCar-v0 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 5
+python -m rl.optimistic_vi.main --env MountainCar-v0 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 10
+python -m rl.optimistic_vi.main --env MountainCar-v0 --optimistic --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 --beta 15
+python -m rl.optimistic_vi.main --env MountainCar-v0 --std-vi --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 
+python -m rl.optimistic_vi.main --env MountainCar-v0 --egreedy --tmp-dir tmp --n-run 50 --buffer 10000 --fourier-order 1 
