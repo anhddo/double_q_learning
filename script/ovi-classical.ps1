a4911a73d87a9a99759ee0a15d0b4ac169fa6cb8
anhdd14 <anhddo93@gmail.com>
2020-09-07 23:30:16 +0700

    update ov classical script

--- script/ovi-classical.ps1
+++ script/ovi-classical.ps1
@@ -1,8 +1,3 @@
-python -m rl.deep_rl.classical.run --tmp-dir tmp --env CartPole-v1 --ddqn --vi --optimistic --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 50000 --epoch-step 600
-#python -m rl.deep_rl.classical.run --tmp-dir tmp --env CartPole-v1 --ddqn --vi --optimistic --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 500000
-#
-#python -m rl.deep_rl.classical.run --tmp-dir tmp --env Acrobot-v1 --ddqn --vi --optimistic --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 50000
-#python -m rl.deep_rl.classical.run --tmp-dir tmp --env Acrobot-v1 --ddqn --vi --optimistic --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 500000
-#
-#python -m rl.deep_rl.classical.run --tmp-dir tmp --env Acrobot-v1 --ddqn --vi --optimistic --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 50000
-#python -m rl.deep_rl.classical.run --tmp-dir tmp --env Acrobot-v1 --ddqn --vi --optimistic --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 500000
+python -m rl.deep_rl.classical.run --tmp-dir tmp --env CartPole-v1 --ddqn --vi --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 50000 --no-explore
+python -m rl.deep_rl.classical.run --tmp-dir tmp --env CartPole-v1 --ddqn --vi --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 50000 --optimistic --beta 5
+python -m rl.deep_rl.classical.run --tmp-dir tmp --env CartPole-v1 --ddqn --vi --huber --rms --n-run 10 --learn-start 32 --latent-buffer-size 50000 --epsilon-greedy
